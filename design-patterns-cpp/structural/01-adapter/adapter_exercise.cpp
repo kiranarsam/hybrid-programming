ae7a2f12e98c8298190f6f7441ae3095d1e05d39
Kiran Kumar Arsam <Kiran.Arsam@users.noreply.gitlab.com>
2025-07-22 18:59:02 +0530

    Adapter design pattern changes

---
+++ design-patterns-cpp/structural/01-adapter/adapter_exercise.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Square
+{
+  int side{ 0 };
+public:
+  explicit Square(const int side) : side(side) {  }
+
+  int getSide() const {
+    return side;
+  }
+};
+
+class Rectangle
+{
+public:
+  virtual int width() const = 0;
+  virtual int height() const = 0;
+
+  int area() const {
+    return width() * height();
+  }
+};
+
+class SquareToRectangleAdapter : public Rectangle
+{
+  const Square& square;
+public:
+  SquareToRectangleAdapter(const Square& square) : square(square) {}
+
+  int width() const override
+  {
+    return square.getSide();
+  }
+
+  int height() const override
+  {
+    return square.getSide();
+  }
+};
+
+int main(int argc, char** argv)
+{
+  Square sq{ 11 };
+  SquareToRectangleAdapter adapter{ sq };
+  std::cout << adapter.area() << std::endl;
+  return 0;
+}
