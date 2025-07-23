7fbcb44880713d8e5319cebc69a3ad78341f8b83
Kiran Kumar Arsam <Kiran.Arsam@users.noreply.gitlab.com>
2025-07-23 22:53:02 +0530

    Removed gtest changes

--- design-patterns-cpp/structural/04-decorator/decorator_exercise.cpp
+++ design-patterns-cpp/structural/04-decorator/decorator_exercise.cpp
@@ -26,12 +26,16 @@ struct RedFlower : Flower
   string str() override
   {
     string s = flower.str();
-    if (s.find("red") != string::npos) return s;
-    else if (s.find("blue") != string::npos)
-    {
+
+    if (s.find("red") != string::npos) {
+      return s;
+    }
+    else if (s.find("blue") != string::npos) {
       return s + " and red";
     }
-    else return s + " that is red";
+    else {
+      return s + " that is red";
+    }
   }
 };
 
@@ -44,71 +48,44 @@ struct BlueFlower : Flower
   string str() override
   {
     string s = flower.str();
-    if (s.find("blue") != string::npos) return s;
-    else if (s.find("red") != string::npos)
-    {
+
+    if (s.find("blue") != string::npos) {
+      return s;
+    }
+    else if (s.find("red") != string::npos) {
       return s + " and blue";
     }
-    else return s + " that is blue";
+    else {
+      return s + " that is blue";
+    }
   }
 };
 
-#include "gtest/gtest.h"
-
-//#include "helpers/iohelper.h"
-
-//#include "exercise.cpp"
-
-namespace
-{
-  class Evaluate : public testing::Test
-  {
-  public:
-    Rose rose;
-    BlueFlower blue_rose{rose};
-    RedFlower red_rose{rose};
-  };
-
-  TEST_F(Evaluate, BaselineTest)
-  {
-    ASSERT_EQ("A rose", Rose{}.str());
-  }
-
-  TEST_F(Evaluate, SingleColorTests)
-  {
-    ASSERT_EQ("A rose that is blue", blue_rose.str());
-    ASSERT_EQ("A rose that is red", red_rose.str());
-  }
-
-  TEST_F(Evaluate, RepetitionTest)
-  {
-    ASSERT_EQ("A rose that is red", RedFlower{red_rose}.str())
-            << "This rose was made red twice, but you should print once.";
-  }
-
-  TEST_F(Evaluate, MultiColorTests)
-  {
-    ASSERT_EQ("A rose that is red and blue",
-      BlueFlower{red_rose}.str());
-    ASSERT_EQ("A rose that is blue and red",
-      RedFlower{blue_rose}.str());
-  }
-
-  TEST_F(Evaluate, NestedRepetitionTest)
-  {
-    BlueFlower b_r_rose{red_rose};
-    RedFlower r_b_r_rose{b_r_rose};
-    ASSERT_EQ("A rose that is red and blue",
-      r_b_r_rose.str())
-      << "This is a complicated case. I'm expecting that "
-      << "a rose defined as Red{Blue{Red{Rose{}}}} is printed "
-      << "as 'red and blue'.";
-  }
-
-} // namespace
-
 int main(int ac, char* av[])
 {
-  testing::InitGoogleTest(&ac, av);
-  return RUN_ALL_TESTS();
+  Rose rose;
+  BlueFlower blue_rose{rose};
+  RedFlower red_rose{rose};
+
+  std::cout << rose.str() << "\n";
+
+  std::cout << blue_rose.str() << "\n";
+
+  std::cout << red_rose.str() << "\n";
+
+  std::cout << RedFlower{red_rose}.str() << " *** "
+            << "This rose was made red twice, but you should print once."  << "\n";
+
+  std::cout << BlueFlower{red_rose}.str() << "\n";
+
+  std::cout << RedFlower{blue_rose}.str()  << "\n";
+
+  BlueFlower b_r_rose{red_rose};
+  RedFlower r_b_r_rose{b_r_rose};
+  std::cout << r_b_r_rose.str() << "\n *** \n"
+    << "This is a complicated case. I'm expecting that "
+    << "a rose defined as Red{Blue{Red{Rose{}}}} is printed "
+    << "as 'red and blue'."  << "\n";
+
+  return 0;
 }
