0eb622f4a379442b7fef98b7a448deb420f89a41
Kiran Kumar Arsam <KiranArsam@users.noreply.gitlab.com>
2026-03-31 10:25:38 +0530

    general programming

---
+++ cpp20-masterclass/sec-10/01-scope.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int global_var1 {23}; // global variable
+
+void some_function() {
+    int local_var{10};
+    std::cout << "Inside function global_var : " << global_var1 << std::endl;
+    std::cout << "Inside functoin local_var : " << local_var << std::endl;
+}
+
+void some_other_function() {
+    //local_var = 5; // can't access local_var from another func
+}
+
+int main() {
+ std::cout << "Inside main function global_var : " << global_var1 << std::endl;
+ //local_var = 5;
+ some_function();
+ return 0;
+}
