b7f4ffb8ef36e3761605a3272c116f05386997c4
Kiran Kumar Arsam <KiranArsam@users.noreply.gitlab.com>
2026-03-31 15:51:38 +0530

    General Programming

---
+++ cpp20-masterclass/sec-15/04-refs-range-for-loop.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int scores[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    std::cout << "Scores : ";
+    for (auto score : scores) {
+        std::cout << " " << score;
+    }
+    std::cout << std::endl;
+
+    for (auto &score : scores) {
+        score = score * 10;
+    }
+
+    // Printing after change
+    std::cout << "Scores : ";
+    for (auto score : scores) {
+        std::cout << " " << score;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
