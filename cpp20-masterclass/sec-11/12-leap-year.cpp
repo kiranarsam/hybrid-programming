93a53d324ebbab3978f2b2057e3c7c4bd7d1f9a2
Kiran Kumar Arsam <KiranArsam@users.noreply.gitlab.com>
2026-03-31 10:24:14 +0530

    general programming

---
+++ cpp20-masterclass/sec-11/12-leap-year.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+bool isLeapYear(unsigned int year) {
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+         is_leap = true;
+    }
+    else {
+        is_leap = false;
+    }
+
+    return is_leap;
+}
+
+int main() {
+
+  std::cout << "Is Leap Year: " << isLeapYear(2100) << std::endl;
+
+  return 0;
+}
